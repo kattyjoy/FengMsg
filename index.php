191a268e5641e10172ecd3f99c3e7eee7f9281f2
joy <isjoychen@hotmail.com>
2015-09-22 22:20:26 +0800

    修改验证

--- index.php
+++ index.php
@@ -1,34 +1,21 @@
-
-    <?php  
-  
-function Valid()  
-{  
-    $echoStr = $_GET["echostr"];  
-       
-    if($this->CheckSignature()){  
-        echo $echoStr;  
-        exit;  
-    }  
+<?php  
+private function checkSignature()
+{
+        $signature = $_GET["signature"];
+        $timestamp = $_GET["timestamp"];
+        $nonce = $_GET["nonce"];    
+                
+    $token = "Feng87Wx";
+    $tmpArr = array($token, $timestamp, $nonce);
+    sort($tmpArr, SORT_STRING);
+    $tmpStr = implode( $tmpArr );
+    $tmpStr = sha1( $tmpStr );
+    
+    if( $tmpStr == $signature ){
+        return true;
+    }else{
+        return false;
+    }
 }  
-  
-function CheckSignature() //验证  
-{  
-    $signature = $_GET["signature"];  
-    $timestamp = $_GET["timestamp"];  
-    $nonce = $_GET["nonce"];  
-  
-    $token = "Feng87Wx";  
-    $tmpArr = array($token, $timestamp, $nonce);  
-    sort($tmpArr);  
-    $tmpStr = implode( $tmpArr );  
-    $tmpStr = sha1( $tmpStr ); //加密  
-       
-    if( $tmpStr == $signature ){  //跟微信服务器传送过来的签名对比  
-        return true;  
-    }else{  
-        return false;  
-    }  
-}  
-  
-Valid();  
+  checkSignature()
 ?>  
